f62a1b39cb42b6e2e41d8809af6880a35f590876
Björn Michael <b.michael@gmx.de>
2016-06-17 07:17:42 +0200

    show folders marked as derived in message dialog within details pane

--- bjmi.derivedresources/src/bjmi/derivedresources/folder/MarkDerivedJob.java
+++ bjmi.derivedresources/src/bjmi/derivedresources/folder/MarkDerivedJob.java
@@ -1,20 +1,25 @@
 package bjmi.derivedresources.folder;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import org.eclipse.core.resources.IFolder;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.resources.WorkspaceJob;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.MultiStatus;
 import org.eclipse.core.runtime.Status;
+import org.eclipse.jface.dialogs.ErrorDialog;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.osgi.util.NLS;
 import org.eclipse.ui.PlatformUI;
 
 import bjmi.derivedresources.core.Logging;
 import bjmi.derivedresources.folder.DerivedFolderVisitor.VisitorStrategy;
-import bjmi.derivedresources.internal.DerivedResourcesMessages;
 import bjmi.derivedresources.internal.DerivedResourcePlugin;
+import bjmi.derivedresources.internal.DerivedResourcesMessages;
 
 /**
  * Search for folders that aren't marked as derived.
@@ -24,97 +29,104 @@ import bjmi.derivedresources.internal.DerivedResourcePlugin;
  */
 public class MarkDerivedJob extends WorkspaceJob {
 
-  /**
-   * @author Björn Michael
-   * @since 1.0
-   */
-  static final class CountAndMarkDerivedStrategy implements VisitorStrategy {
+    /**
+     * @author Björn Michael
+     * @since 1.0
+     */
+    static final class CountAndMarkDerivedStrategy implements VisitorStrategy {
 
-    private final IProgressMonitor monitor;
-    private int count = 0;
+        private final IProgressMonitor monitor;
+        private final List<String> paths = new LinkedList<String>();
 
-    CountAndMarkDerivedStrategy(final IProgressMonitor progressMonitor) {
-      monitor = progressMonitor;
+        CountAndMarkDerivedStrategy(final IProgressMonitor progressMonitor) {
+            monitor = progressMonitor;
+        }
+
+        @Override
+        public void apply(final IFolder folder) {
+            try {
+                folder.setDerived(true, monitor);
+                paths.add(folder.getFullPath().toString());
+            } catch (final CoreException exc) {
+                asyncExec(new Runnable() {
+                    @Override
+                    public void run() {
+                        MessageDialog.openError(null, DerivedResourcesMessages.MarkDerivedAction_Error_Title,
+                                        NLS.bind(DerivedResourcesMessages.MarkDerivedAction_Error_Message, exc.getMessage()));
+                    }
+                });
+            }
+        }
+
+    }
+
+    private final Iterable<String> folderNames;
+    private CancelableVisitor visitor;
+
+    private MarkDerivedJob(final String jobName, final Iterable<String> folderNames) {
+        super(jobName);
+        this.folderNames = folderNames;
+    }
+
+    /**
+     * Creates and schedules a new job to be run.
+     *
+     * @param folderNames
+     *            that should be marked as derived
+     */
+    public static synchronized void schedule(final Iterable<String> folderNames) {
+        new MarkDerivedJob(DerivedResourcesMessages.MarkDerivedJob_Job_Name, folderNames).schedule();
+    }
+
+    private static void asyncExec(final Runnable runnable) {
+        PlatformUI.getWorkbench().getDisplay().asyncExec(runnable);
     }
 
     @Override
-    public void apply(final IFolder folder) {
-      try {
-        folder.setDerived(true, monitor);
-        count++;
-      } catch (final CoreException exc) {
-        asyncExec(new Runnable() {
-          @Override
-          public void run() {
-            MessageDialog.openError(null, DerivedResourcesMessages.MarkDerivedAction_Error_Title,
-                NLS.bind(DerivedResourcesMessages.MarkDerivedAction_Error_Message, exc.getMessage()));
-          }
-        });
-      }
-    }
+    public IStatus runInWorkspace(final IProgressMonitor monitor) {
+        monitor.beginTask(DerivedResourcesMessages.MarkDerivedJob_Search, IProgressMonitor.UNKNOWN);
 
-  }
-
-  private final Iterable<String> folderNames;
-  private CancelableVisitor visitor;
-
-  private MarkDerivedJob(final String jobName, final Iterable<String> folderNames) {
-    super(jobName);
-    this.folderNames = folderNames;
-  }
-
-  /**
-   * Creates and schedules a new job to be run.
-   *
-   * @param folderNames
-   *          that should be marked as derived
-   */
-  public static synchronized void schedule(final Iterable<String> folderNames) {
-    new MarkDerivedJob(DerivedResourcesMessages.MarkDerivedJob_Job_Name, folderNames).schedule();
-  }
-
-  private static void asyncExec(final Runnable runnable) {
-    PlatformUI.getWorkbench().getDisplay().asyncExec(runnable);
-  }
-
-  @Override
-  public IStatus runInWorkspace(final IProgressMonitor monitor) {
-    monitor.beginTask(DerivedResourcesMessages.MarkDerivedJob_Search, IProgressMonitor.UNKNOWN);
-
-    final CountAndMarkDerivedStrategy strategy = new CountAndMarkDerivedStrategy(monitor);
-    try {
-      visitor = new CancelableVisitor(new LoggingVisitor(new DerivedFolderVisitor(folderNames, strategy), monitor));
-      ResourcesPlugin.getWorkspace().getRoot().accept(visitor);
-    } catch (final CoreException exc) {
-      Logging.log(DerivedResourcePlugin.getInstance(), exc.getStatus());
-      // TODO reconcile messages
-      // MessageDialog.openError(null, DerivedResoucesMessages.MarkDerivedAction_Error_Title,
-      // NLS.bind(DerivedResoucesMessages.MarkDerivedAction_Error_Message, exc.getMessage()));
-      return exc.getStatus();
-    } finally {
-      monitor.setTaskName(DerivedResourcesMessages.MarkDerivedJob_Search_Complete);
-      monitor.done();
-      visitor = null;
-    }
-    if (strategy.count > 0) {
-      asyncExec(new Runnable() {
-        @Override
-        public void run() {
-          MessageDialog.openInformation(null, DerivedResourcesMessages.MarkDerivedAction_Result_Title,
-              NLS.bind(DerivedResourcesMessages.MarkDerivedAction_Result_Message, strategy.count));
+        final CountAndMarkDerivedStrategy strategy = new CountAndMarkDerivedStrategy(monitor);
+        try {
+            visitor = new CancelableVisitor(new LoggingVisitor(new DerivedFolderVisitor(folderNames, strategy), monitor));
+            ResourcesPlugin.getWorkspace().getRoot().accept(visitor);
+        } catch (final CoreException exc) {
+            Logging.log(DerivedResourcePlugin.getInstance(), exc.getStatus());
+            // TODO reconcile messages
+            // MessageDialog.openError(null, DerivedResoucesMessages.MarkDerivedAction_Error_Title,
+            // NLS.bind(DerivedResoucesMessages.MarkDerivedAction_Error_Message, exc.getMessage()));
+            return exc.getStatus();
+        } finally {
+            monitor.setTaskName(DerivedResourcesMessages.MarkDerivedJob_Search_Complete);
+            monitor.done();
+            visitor = null;
         }
-      });
+        if (!strategy.paths.isEmpty()) {
+            asyncExec(new Runnable() {
+                @Override
+                public void run() {
+                    MultiStatus ms = new MultiStatus(DerivedResourcePlugin.PLUGIN_ID, IStatus.INFO,
+                                    NLS.bind(DerivedResourcesMessages.MarkDerivedAction_Result_Message, strategy.paths.size()), null);
+                    for (String path : strategy.paths) {
+                        ms.add(new Status(IStatus.INFO, DerivedResourcePlugin.PLUGIN_ID, path));
+                    }
+                    ErrorDialog.openError(null, DerivedResourcesMessages.MarkDerivedAction_Result_Title, //
+                                    null, // set null to show message from multistatus
+                                    ms);
+                }
+            });
+        }
+        return Status.OK_STATUS;
     }
-    return Status.OK_STATUS;
-  }
 
-  @Override
-  protected void canceling() {
-    if (visitor != null) {
-      final Status status = new Status(IStatus.CANCEL, DerivedResourcePlugin.PLUGIN_ID, DerivedResourcesMessages.MarkDerivedJob_Cancel);
-      visitor.cancelWith(status);
+    @Override
+    protected void canceling() {
+        if (visitor != null) {
+            final Status status = new Status(IStatus.CANCEL, DerivedResourcePlugin.PLUGIN_ID,
+                            DerivedResourcesMessages.MarkDerivedJob_Cancel);
+            visitor.cancelWith(status);
+        }
+        super.canceling();
     }
-    super.canceling();
-  }
 
 }
--- bjmi.derivedresources/src/bjmi/derivedresources/folder/SearchDerivedFolderJob.java
+++ bjmi.derivedresources/src/bjmi/derivedresources/folder/SearchDerivedFolderJob.java
@@ -18,8 +18,8 @@ import org.eclipse.ui.handlers.IHandlerService;
 
 import bjmi.derivedresources.core.Logging;
 import bjmi.derivedresources.folder.DerivedFolderVisitor.VisitorStrategy;
-import bjmi.derivedresources.internal.DerivedResourcesMessages;
 import bjmi.derivedresources.internal.DerivedResourcePlugin;
+import bjmi.derivedresources.internal.DerivedResourcesMessages;
 import bjmi.derivedresources.preferences.Preferences;
 
 /**
@@ -31,99 +31,99 @@ import bjmi.derivedresources.preferences.Preferences;
 // XXX rename to searchderivedfoldersjob
 public class SearchDerivedFolderJob extends WorkspaceJob implements VisitorStrategy {
 
-  private static SearchDerivedFolderJob instance;
+    private static SearchDerivedFolderJob instance;
 
-  private final Iterable<String> folderNames;
-  private CancelableVisitor visitor;
+    private final Iterable<String> folderNames;
+    private CancelableVisitor visitor;
 
-  private SearchDerivedFolderJob(final String jobName, final Iterable<String> folderNames) {
-    super(jobName);
-    this.folderNames = folderNames;
+    private SearchDerivedFolderJob(final String jobName, final Iterable<String> folderNames) {
+        super(jobName);
+        this.folderNames = folderNames;
 
-    // hide this job from user in the UI
-    setSystem(true);
-  }
-
-  /**
-   * Schedules this job to be run. An existing job will be canceled and a new job will be created.
-   *
-   * @param folderNames
-   *          that should be marked as derived.
-   */
-  public static synchronized void schedule(final Iterable<String> folderNames) {
-    if (instance != null) {
-      instance.cancel();
+        // hide this job from user in the UI
+        setSystem(true);
     }
 
-    instance = new SearchDerivedFolderJob(DerivedResourcesMessages.MarkDerivedJob_Job_Name, folderNames);
-    instance.schedule();
-  }
-
-  private static void aSyncExec(final Runnable runnable) {
-    PlatformUI.getWorkbench().getDisplay().syncExec(runnable);
-  }
-
-  private static void openDialog() {
-    aSyncExec(new Runnable() {
-      @Override
-      public void run() {
-        final IHandlerService handlerService = (IHandlerService) PlatformUI.getWorkbench().getService(IHandlerService.class);
-        try {
-          handlerService.executeCommand(MarkDerivedHandler.COMMAND_ID, null);
-        } catch (final ExecutionException exc) {
-          Logging.log(DerivedResourcePlugin.getInstance(), new Status(IStatus.ERROR, DerivedResourcePlugin.PLUGIN_ID,
-              "Command execution failed", exc));
-        } catch (final NotDefinedException exc) {
-          Logging.log(DerivedResourcePlugin.getInstance(), new Status(IStatus.ERROR, DerivedResourcePlugin.PLUGIN_ID,
-              "Command not defined", exc));
-        } catch (final NotEnabledException exc) {
-          Logging.log(DerivedResourcePlugin.getInstance(), new Status(IStatus.ERROR, DerivedResourcePlugin.PLUGIN_ID,
-              "Command not enabled", exc));
-        } catch (final NotHandledException exc) {
-          Logging.log(DerivedResourcePlugin.getInstance(), new Status(IStatus.ERROR, DerivedResourcePlugin.PLUGIN_ID,
-              "Command not handled", exc));
+    /**
+     * Schedules this job to be run. An existing job will be canceled and a new job will be created.
+     *
+     * @param folderNames
+     *            that should be marked as derived.
+     */
+    public static synchronized void schedule(final Iterable<String> folderNames) {
+        if (instance != null) {
+            instance.cancel();
         }
 
-      }
-    });
-  }
-
-  @Override
-  public void apply(final IFolder folder) {
-    cancel();
-    openDialog();
-  }
-
-  @Override
-  public IStatus runInWorkspace(final IProgressMonitor monitor) {
-    monitor.beginTask(DerivedResourcesMessages.MarkDerivedJob_Search, IProgressMonitor.UNKNOWN);
-    try {
-      visitor = new CancelableVisitor(new LoggingVisitor(new DerivedFolderVisitor(folderNames, this), monitor));
-      ResourcesPlugin.getWorkspace().getRoot().accept(visitor);
-    } catch (final CoreException exc) {
-      Logging.log(DerivedResourcePlugin.getInstance(), exc.getStatus());
-      // TODO reconcile messages
-      // MessageDialog.openError(null, DerivedResoucesMessages.MarkDerivedAction_Error_Title,
-      // NLS.bind(DerivedResoucesMlessages.MarkDerivedAction_Error_Message, exc.getMessage()));
-      return exc.getStatus();
-    } finally {
-      monitor.setTaskName(DerivedResourcesMessages.MarkDerivedJob_Search_Complete);
-      monitor.done();
-      visitor = null;
-      if (Preferences.SCAN_IN_BACKGROUND.getValue()) {
-        schedule(TimeUnit.SECONDS.toMillis(Preferences.SCAN_IN_BACKGROUND_PERIOD.getValue()));
-      }
+        instance = new SearchDerivedFolderJob(DerivedResourcesMessages.MarkDerivedJob_Job_Name, folderNames);
+        instance.schedule();
     }
-    return Status.OK_STATUS;
-  }
 
-  @Override
-  protected void canceling() {
-    if (visitor != null) {
-      final Status status = new Status(IStatus.CANCEL, DerivedResourcePlugin.PLUGIN_ID, "Cancelling mark folders as derived job.");
-      visitor.cancelWith(status);
+    private static void aSyncExec(final Runnable runnable) {
+        PlatformUI.getWorkbench().getDisplay().syncExec(runnable);
     }
-    super.canceling();
-  }
+
+    private static void openDialog() {
+        aSyncExec(new Runnable() {
+            @Override
+            public void run() {
+                final IHandlerService handlerService = PlatformUI.getWorkbench().getService(IHandlerService.class);
+                try {
+                    handlerService.executeCommand(MarkDerivedHandler.COMMAND_ID, null);
+                } catch (final ExecutionException exc) {
+                    Logging.log(DerivedResourcePlugin.getInstance(),
+                                    new Status(IStatus.ERROR, DerivedResourcePlugin.PLUGIN_ID, "Command execution failed", exc));
+                } catch (final NotDefinedException exc) {
+                    Logging.log(DerivedResourcePlugin.getInstance(),
+                                    new Status(IStatus.ERROR, DerivedResourcePlugin.PLUGIN_ID, "Command not defined", exc));
+                } catch (final NotEnabledException exc) {
+                    Logging.log(DerivedResourcePlugin.getInstance(),
+                                    new Status(IStatus.ERROR, DerivedResourcePlugin.PLUGIN_ID, "Command not enabled", exc));
+                } catch (final NotHandledException exc) {
+                    Logging.log(DerivedResourcePlugin.getInstance(),
+                                    new Status(IStatus.ERROR, DerivedResourcePlugin.PLUGIN_ID, "Command not handled", exc));
+                }
+
+            }
+        });
+    }
+
+    @Override
+    public void apply(final IFolder folder) {
+        cancel();
+        openDialog();
+    }
+
+    @Override
+    public IStatus runInWorkspace(final IProgressMonitor monitor) {
+        monitor.beginTask(DerivedResourcesMessages.MarkDerivedJob_Search, IProgressMonitor.UNKNOWN);
+        try {
+            visitor = new CancelableVisitor(new LoggingVisitor(new DerivedFolderVisitor(folderNames, this), monitor));
+            ResourcesPlugin.getWorkspace().getRoot().accept(visitor);
+        } catch (final CoreException exc) {
+            Logging.log(DerivedResourcePlugin.getInstance(), exc.getStatus());
+            // TODO reconcile messages
+            // MessageDialog.openError(null, DerivedResoucesMessages.MarkDerivedAction_Error_Title,
+            // NLS.bind(DerivedResoucesMlessages.MarkDerivedAction_Error_Message, exc.getMessage()));
+            return exc.getStatus();
+        } finally {
+            monitor.setTaskName(DerivedResourcesMessages.MarkDerivedJob_Search_Complete);
+            monitor.done();
+            visitor = null;
+            if (Preferences.SCAN_IN_BACKGROUND.getValue()) {
+                schedule(TimeUnit.SECONDS.toMillis(Preferences.SCAN_IN_BACKGROUND_PERIOD.getValue()));
+            }
+        }
+        return Status.OK_STATUS;
+    }
+
+    @Override
+    protected void canceling() {
+        if (visitor != null) {
+            final Status status = new Status(IStatus.CANCEL, DerivedResourcePlugin.PLUGIN_ID, "Cancelling mark folders as derived job.");
+            visitor.cancelWith(status);
+        }
+        super.canceling();
+    }
 
 }
