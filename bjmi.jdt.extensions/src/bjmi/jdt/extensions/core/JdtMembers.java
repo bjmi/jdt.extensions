fd6cffcc50d451e532e0498a882229a7c513cb7f
Björn Michael <b.michael@gmx.de>
2019-10-09 13:30:32 +0200

    Remove filters that were migrated to Eclipse SDK

--- bjmi.jdt.extensions/src/bjmi/jdt/extensions/core/JdtMembers.java
+++ bjmi.jdt.extensions/src/bjmi/jdt/extensions/core/JdtMembers.java
@@ -5,10 +5,8 @@ import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Plugin;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.jdt.core.Flags;
-import org.eclipse.jdt.core.IClassFile;
 import org.eclipse.jdt.core.IJavaElement;
 import org.eclipse.jdt.core.IMember;
-import org.eclipse.jdt.core.IType;
 import org.eclipse.jdt.core.JavaModelException;
 
 /**
@@ -19,59 +17,30 @@ import org.eclipse.jdt.core.JavaModelException;
  */
 public final class JdtMembers {
 
-  private JdtMembers() {
-    throw new AssertionError("invocation of sealed constructor"); // prevent instantiation
-  }
+	private JdtMembers() {
+		throw new AssertionError("invocation of sealed constructor"); // Prevent instantiation
+	}
 
-  /**
-   * @param member
-   *          e.g. method or field in a java class
-   * @return true if the element is marked as deprecated
-   */
-  public static boolean isDeprecated(final IMember member) {
-    switch (member.getElementType()) {
-      case IJavaElement.FIELD :
-      case IJavaElement.METHOD :
-        try {
-          return Flags.isDeprecated(member.getFlags());
-        } catch (final JavaModelException e) {
-          final Plugin instance = JdtExtensionsPlugin.getInstance();
-          final Status status = new Status(IStatus.INFO, instance.getBundle().getSymbolicName(),
-              "can't determine if member is deprecated due unretrievable flags", e);
+	/**
+	 * @param member e.g. method or field in a java class
+	 * @return true if the element is marked as deprecated
+	 */
+	public static boolean isDeprecated(final IMember member) {
+		switch (member.getElementType()) {
+		case IJavaElement.FIELD:
+		case IJavaElement.METHOD:
+			try {
+				return Flags.isDeprecated(member.getFlags());
+			} catch (final JavaModelException e) {
+				final Plugin instance = JdtExtensionsPlugin.getInstance();
+				final Status status = new Status(IStatus.INFO, instance.getBundle().getSymbolicName(),
+						"can't determine if member is deprecated due unretrievable flags", e);
 
-          instance.getLog().log(status);
-        }
-    }
-    return false;
-  }
-
-  /**
-   * @param member of a java class
-   * @return true if {@code member} stems from {@link java.lang.Object}
-   * @since 1.2
-   */
-  public static boolean isFromJavaLangObject(final IMember member) {
-    switch (member.getElementType()) {
-    case IJavaElement.METHOD :
-        System.out.println(member);
-        IClassFile classFile = member.getClassFile();
-        if (classFile != null) {
-          IType type = classFile.getType();
-          String fullyQualifiedName = type.getFullyQualifiedName();
-          if ("java.lang.Object".equals(fullyQualifiedName)) {
-            return true;
-          }
-        }
-      }
-      /*
-      catch (final JavaModelException e) {
-        final Plugin instance = JdtExtensionsPlugin.getInstance();
-        final Status status = new Status(IStatus.ERROR, instance.getBundle().getSymbolicName(), "type could not be inferred", e);
-
-        instance.getLog().log(status);
-      }
-      */
-    return false;
-  }
+				instance.getLog().log(status);
+			}
+		}
+		
+		return false;
+	}
 
 }
